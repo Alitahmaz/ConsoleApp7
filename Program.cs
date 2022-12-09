cc861ab990ea6e9f2a368fb31204ff3d1120600a
Acsescity <Acsescity@DESKTOP-NONREC2>
2022-12-09 20:07:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            number1 = Convert.ToInt32(Console.ReadLine());
+            number2 = Convert.ToInt32(Console.ReadLine());
+            int c = number1 % number2;
+            Console.WriteLine(c);
+        }
+    }
+}
